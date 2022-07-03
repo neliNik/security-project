b42deae15d88224a716ecc8368c144262c270160
Neli Nikolova <radkova_neli@abv.bg>
2022-07-03 23:04:06 +0300

    new security project

---
+++ user-auth/src/main/java/com/example/userauth/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.example.userauth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class UserController {
+   
+	@GetMapping("/user")
+	@ResponseBody
+	public String showUser() {
+		 
+		return "user";
+	}
+}
